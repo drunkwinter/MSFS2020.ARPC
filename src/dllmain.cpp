2eff99f3000ee70954e39fb08aab0a32c2c0798a
drunkwinter <38593134+drunkwinter@users.noreply.github.com>
2023-10-09 05:30:44 +0200

    [feat] add configuration

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,7 +1,10 @@
 #define WIN32_LEAN_AND_MEAN
 
+#pragma comment(lib, "user32.lib")
+
 #include <windows.h>
 #include "Hooking.Patterns\Hooking.Patterns.h"
+#include "IniReader\IniReader.h"
 
 void Init() {
     auto pattern = hook::pattern("C3 64 2A 3A E3 8B F6 3A 07 42 B2 38");
@@ -11,9 +14,41 @@ void Init() {
         return;
     }
 
-    *reinterpret_cast<float*>(pattern.get_first()) = 0.0027234f;
-    *reinterpret_cast<float*>(pattern.get_first(4)) = 0.001831f;
-    *reinterpret_cast<float*>(pattern.get_first(8)) = -0.000083096f;
+    CIniReader iniReader("");
+
+    const auto pCoefficients = pattern.get_first<float>();
+    const float fCoefficients[] = {pCoefficients[0], pCoefficients[1], pCoefficients[2]};
+    const float fNewCoefficients[] = {
+        iniReader.ReadFloat("COEFFICIENTS", "RED", 0.002723f),
+        iniReader.ReadFloat("COEFFICIENTS", "GREEN", 0.001831f),
+        iniReader.ReadFloat("COEFFICIENTS", "BLUE", -0.000083096f),
+    };
+
+    std::copy(std::begin(fNewCoefficients), std::end(fNewCoefficients), pCoefficients);
+
+    const auto bToggleEnabled = iniReader.ReadBoolean("TOGGLE", "ENABLED", FALSE);
+
+    if (bToggleEnabled) {
+        const auto key = iniReader.ReadInteger("TOGGLE", "KEY_CODE", 0x2D);
+        const auto modifier = MOD_NOREPEAT
+            | (iniReader.ReadBoolean("TOGGLE", "HOLD_ALT", FALSE) * MOD_ALT)
+            | (iniReader.ReadBoolean("TOGGLE", "HOLD_CTRL", FALSE) * MOD_CONTROL)
+            | (iniReader.ReadBoolean("TOGGLE", "HOLD_SHIFT", FALSE) * MOD_SHIFT);
+
+        RegisterHotKey(NULL, 1, modifier, key);
+
+        bool bPatchEnabled = true;
+        MSG msg = {0};
+        while (GetMessage(&msg, NULL, 0, 0) != 0)
+        {
+            if (msg.message != WM_HOTKEY)
+                continue;
+
+            const auto _fCoefficients = bPatchEnabled ? &fCoefficients : &fNewCoefficients;
+            std::copy(std::begin(*_fCoefficients), std::end(*_fCoefficients), pCoefficients);
+            bPatchEnabled = !bPatchEnabled;
+        }
+    }
 }
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
